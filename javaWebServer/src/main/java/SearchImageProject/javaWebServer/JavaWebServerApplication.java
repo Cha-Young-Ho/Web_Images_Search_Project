df882b50523754feff93033acf93032fc7330b30
SSP_Project <jkijki1249@gmail.com>
2021-09-27 16:05:07 +0900

    java web server 프로젝트 파일 새로 생성

---
+++ javaWebServer/src/main/java/SearchImageProject/javaWebServer/JavaWebServerApplication.java
@@ -0,0 +1,13 @@
+package SearchImageProject.javaWebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebServerApplication.class, args);
+	}
+
+}
---
+++ javaWebServer/src/test/java/SearchImageProject/javaWebServer/JavaWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package SearchImageProject.javaWebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
